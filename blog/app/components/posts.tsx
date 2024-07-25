64123dbb5999e18cf282a627efc6d3440b4520b5
joshieyu <jyemails1@gmail.com>
2024-07-25 17:06:46 -0400

    Started blog

--- blog/app/components/posts.tsx
+++ blog/app/components/posts.tsx
@@ -1,37 +1,40 @@
-import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import Link from 'next/link';
+import { formatDate, getBlogPosts } from 'app/blog/utils';
 
 export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+  let allBlogs = getBlogPosts();
 
   return (
     <div>
       {allBlogs
         .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1
-          }
-          return 1
+          if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) return -1;
+          return 1;
         })
         .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
+          <Link key={post.slug} className="flex flex-col space-y-1 mb-4" href={`/blog/${post.slug}`}>
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-                {/* {post.metadata.summary} */}
-              </p>
+              <div className="flex flex-col">
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {post.metadata.title}
+                </p>
+                {post.metadata.image && (
+                  <img
+                    src={post.metadata.image}
+                    alt={post.metadata.title}
+                    className="w-full h-auto object-cover mb-2"
+                  />
+                )}
+                {/* <p>{post.metadata.summary}</p> */}
+              </div>
             </div>
           </Link>
         ))}
     </div>
-  )
+  );
 }
+
+// export default BlogPosts;
--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -52,15 +52,16 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+      <body className="antialiased max-w-4xl mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-4">
+        <Navbar />
           {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
+        <footer />
+        <Analytics />
+        <SpeedInsights />
         </main>
-      </body>
+    </body>
+
     </html>
   )
 }
